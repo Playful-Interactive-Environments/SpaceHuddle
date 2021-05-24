eba8c13ce76a4dc5d16206af902cb60e5f67c58e
Melanie Meindl <s2010629006@students.fh-hagenberg.at>
2021-05-24 21:10:07 +0200

    vue project setup

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Hello World <b> {{ msg }} </b>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    msg: String,
+  },
+})
+export default class HelloWorld extends Vue {
+  msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
