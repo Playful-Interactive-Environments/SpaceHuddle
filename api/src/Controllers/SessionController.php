503ff94815e8e96a27969005c299a2483bfee14f
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2021-05-27 23:09:13 +0200

    Refactored SessionController
    Code conventions and PHPdoc.

--- api/src/Controllers/SessionController.php
+++ api/src/Controllers/SessionController.php
@@ -1,20 +1,29 @@
 <?php
+
 namespace PieLab\GAB\Controllers;
 
 use PieLab\GAB\Config\Authorization;
 use PieLab\GAB\Config\Generator;
-
-use PieLab\GAB\Models\Session;
 use PieLab\GAB\Models\Role;
+use PieLab\GAB\Models\Session;
 
+/**
+ * Controller for sessions.
+ * @package PieLab\GAB\Controllers
+ */
 class SessionController extends AbstractController
 {
-    public function __construct()
+    /**
+     * Creates a new SessionController.
+     */
+    protected function __construct()
     {
         parent::__construct("session", Session::class);
     }
 
     /**
+     * List all the sessions for which the user is authorized.
+     * @return string A list of sessions in JSON format.
      * @OA\Get(
      *   path="/api/sessions/",
      *   summary="List of all sessions for which the user is authorized.",
@@ -32,25 +41,27 @@ class SessionController extends AbstractController
      */
     public function readAll(): string
     {
-        $login_id = Authorization::getAuthorizationProperty("login_id");
-        $query = "SELECT * FROM session
-      INNER JOIN session_role ON session_role.session_id = session.id
-      WHERE session_role.login_id = :login_id";
+        $loginId = Authorization::getAuthorizationProperty("login_id");
+        $query = "SELECT * FROM session INNER JOIN session_role ON session_role.session_id = session.id
+                  WHERE session_role.login_id = :login_id";
         /*$query = "SELECT * FROM session
           WHERE id IN (SELECT session_id FROM session_role WHERE login_id = :login_id)";*/
-        $stmt = $this->connection->prepare($query);
-        $stmt->bindParam(":login_id", $login_id);
-        $stmt->execute();
-        $result_data = $this->database->fetchAll($stmt);
-        $result = array();
-        foreach ($result_data as $result_item) {
-            array_push($result, new Session($result_item));
+        $statement = $this->connection->prepare($query);
+        $statement->bindParam(":login_id", $loginId);
+        $statement->execute();
+        $resultData = $this->database->fetchAll($statement);
+        $result = [];
+        foreach ($resultData as $resultItem) {
+            array_push($result, new Session($resultItem));
         }
         http_response_code(200);
         return json_encode($result);
     }
 
     /**
+     * Read detail data for the session with the specified id.
+     * @param string|null $id The session ID.
+     * @return string Detailed session data in JSON format.
      * @OA\Get(
      *   path="/api/session/{id}/",
      *   summary="Detail data for the session with the specified id.",
@@ -65,49 +76,60 @@ class SessionController extends AbstractController
      */
     public function read(?string $id = null): string
     {
-        $login_id = Authorization::getAuthorizationProperty("login_id");
+        $loginId = Authorization::getAuthorizationProperty("login_id");
         if (is_null($id)) {
             $id = $this->getUrlParameter("session", -1);
         }
-        $query = "SELECT * FROM session
-      INNER JOIN session_role ON session_role.session_id = session.id
-      WHERE session.id = :id and session_role.login_id = :login_id";
-        $stmt = $this->connection->prepare($query);
-        $stmt->bindParam(":id", $id);
-        $stmt->bindParam(":login_id", $login_id);
-        $stmt->execute();
-        $result = $this->database->fetchFirst($stmt);
+        $query = "SELECT * FROM session INNER JOIN session_role ON session_role.session_id = session.id
+                  WHERE session.id = :id and session_role.login_id = :login_id";
+        $statement = $this->connection->prepare($query);
+        $statement->bindParam(":id", $id);
+        $statement->bindParam(":login_id", $loginId);
+        $statement->execute();
+        $result = $this->database->fetchFirst($statement);
         http_response_code(200);
         return json_encode(new Session($result));
     }
 
-    public function readByKey(string $session_key): object
+    /**
+     * Read data from a session specified by a session key.
+     * @param string $sessionKey
+     * @return object The session data.
+     */
+    public function readByKey(string $sessionKey): object
     {
         $query = "SELECT * FROM session WHERE connection_key = :session_key";
-        $stmt = $this->connection->prepare($query);
-        $stmt->bindParam(":session_key", $session_key);
-        $stmt->execute();
-        return (object)$this->database->fetchFirst($stmt, "Wrong Session Key");
+        $statement = $this->connection->prepare($query);
+        $statement->bindParam(":session_key", $sessionKey);
+        $statement->execute();
+        return (object)$this->database->fetchFirst($statement, "Wrong session key.");
     }
-
-
-    private function generateNewSessionKey(): string
-    {
-        $item_count = 1;
-        $connection_key = "";
-        while ($item_count > 0) {
-            $connection_key = Generator::keygen(8, false);
-            $query = "SELECT id FROM session WHERE connection_key = :key";
-            $stmt = $this->connection->prepare($query);
-            $stmt->bindParam(":key", $connection_key);
-            $stmt->execute();
-            $item_count = $stmt->rowCount();
-        }
-        return $connection_key;
-    }
-
 
     /**
+     * Generate a new session key.
+     * @return string The session key.
+     */
+    private function generateNewSessionKey(): string
+    {
+        $itemCount = 1;
+        $connectionKey = "";
+        while ($itemCount > 0) {
+            $connectionKey = Generator::keygen(8, false);
+            $query = "SELECT id FROM session WHERE connection_key = :key";
+            $statement = $this->connection->prepare($query);
+            $statement->bindParam(":key", $connectionKey);
+            $statement->execute();
+            $itemCount = $statement->rowCount();
+        }
+        return $connectionKey;
+    }
+
+    /**
+     * Create a new session.
+     * @param string|null $title The session title.
+     * @param string|null $maxParticipants The maximum number of participants.
+     * @param string|null $expirationDate The session's expiration date.
+     * @return string The session data in JSON format.
      * @OA\Post(
      *   path="/api/session/",
      *   summary="Create a new session.",
@@ -129,40 +151,47 @@ class SessionController extends AbstractController
      *   security={{"api_key": {}}, {"bearerAuth": {}}}
      * )
      */
-    public function add(
-        ?string $title = null,
-        ?string $max_participants = null,
-        ?string $expiration_date = null
-    ): string {
-        $login_id = Authorization::getAuthorizationProperty("login_id"); # check if the user is logged in
-        $connection_key = $this->generateNewSessionKey();
+    public function add(?string $title = null, ?string $maxParticipants = null, ?string $expirationDate = null): string
+    {
+        $loginId = Authorization::getAuthorizationProperty("login_id"); # check if the user is logged in
+        $connectionKey = $this->generateNewSessionKey();
         $params = $this->formatParameters(
             [
                 "title" => ["default" => $title],
-                "connection_key" => ["default" => $connection_key],
-                "max_participants" => ["default" => $max_participants],
-                "expiration_date" => ["default" => $expiration_date]
+                "connection_key" => ["default" => $connectionKey],
+                "max_participants" => ["default" => $maxParticipants],
+                "expiration_date" => ["default" => $expirationDate]
             ]
         );
 
         return $this->addGeneric(null, $params);
     }
 
+    /**
+     * Include dependent data.
+     * @param string $id Primary key of the linked table entry
+     * @param array|object|null $parameter Dependent data to be included.
+     */
     protected function addDependencies(string $id, array|object|null $parameter)
     {
-        $login_id = Authorization::getAuthorizationProperty("login_id");
+        $loginId = Authorization::getAuthorizationProperty("login_id");
         $role = strtoupper(Role::MODERATOR);
-        $query = "INSERT INTO session_role
-      (session_id, login_id, role)
-      VALUES (:session_id, :login_id, :role)";
-        $stmt = $this->connection->prepare($query);
-        $stmt->bindParam(":session_id", $id);
-        $stmt->bindParam(":login_id", $login_id);
-        $stmt->bindParam(":role", $role);
-        $stmt->execute();
+        $query = "INSERT INTO session_role (session_id, login_id, role) VALUES (:session_id, :login_id, :role)";
+        $statement = $this->connection->prepare($query);
+        $statement->bindParam(":session_id", $id);
+        $statement->bindParam(":login_id", $loginId);
+        $statement->bindParam(":role", $role);
+        $statement->execute();
     }
 
     /**
+     * Update a session.
+     * @param string|null $id The session ID.
+     * @param string|null $title The session title.
+     * @param int|null $maxParticipants The maximum number of participants.
+     * @param string|null $expirationDate The session's expiration daten.
+     * @param string|null $publicScreenModuleId The public screen module's ID.
+     * @return string The updated session data in JSON format.
      * @OA\Put(
      *   path="/api/session/",
      *   summary="Update a session.",
@@ -189,17 +218,17 @@ class SessionController extends AbstractController
     public function update(
         ?string $id = null,
         ?string $title = null,
-        ?int $max_participants = null,
-        ?string $expiration_date = null,
-        ?string $public_screen_module_id = null
+        ?int $maxParticipants = null,
+        ?string $expirationDate = null,
+        ?string $publicScreenModuleId = null
     ): string {
         $params = $this->formatParameters(
             [
                 "id" => ["default" => $id],
                 "title" => ["default" => $title],
-                "max_participants" => ["default" => $max_participants],
-                "expiration_date" => ["default" => $expiration_date],
-                "public_screen_module_id" => ["default" => $public_screen_module_id]
+                "max_participants" => ["default" => $maxParticipants],
+                "expiration_date" => ["default" => $expirationDate],
+                "public_screen_module_id" => ["default" => $publicScreenModuleId]
             ]
         );
 
@@ -214,32 +243,30 @@ class SessionController extends AbstractController
     public function checkRights(?string $id): ?string
     {
         if (!Authorization::isParticipant()) {
-            $login_id = Authorization::getAuthorizationProperty("login_id");
+            $loginId = Authorization::getAuthorizationProperty("login_id");
             if (is_null($id)) {
                 return strtoupper(Role::MODERATOR);
             }
-            $query = "SELECT * FROM session_role
-        WHERE session_id = :session_id AND login_id = :login_id";
-            $stmt = $this->connection->prepare($query);
-            $stmt->bindParam(":session_id", $id);
-            $stmt->bindParam(":login_id", $login_id);
-            $stmt->execute();
-            $item_count = $stmt->rowCount();
-            if ($item_count > 0) {
-                $result = $this->database->fetchFirst($stmt);
+            $query = "SELECT * FROM session_role WHERE session_id = :session_id AND login_id = :login_id";
+            $statement = $this->connection->prepare($query);
+            $statement->bindParam(":session_id", $id);
+            $statement->bindParam(":login_id", $loginId);
+            $statement->execute();
+            $itemCount = $statement->rowCount();
+            if ($itemCount > 0) {
+                $result = $this->database->fetchFirst($statement);
                 return strtoupper($result["role"]);
             }
         } else {
-            $participant_id = Authorization::getAuthorizationProperty("participant_id");
-            $query = "SELECT * FROM participant
-        WHERE session_id = :session_id AND id = :participant_id";
-            $stmt = $this->connection->prepare($query);
-            $stmt->bindParam(":session_id", $id);
-            $stmt->bindParam(":participant_id", $participant_id);
-            $stmt->execute();
-            $item_count = $stmt->rowCount();
-            if ($item_count > 0) {
-                $result = $this->database->fetchFirst($stmt);
+            $participantId = Authorization::getAuthorizationProperty("participant_id");
+            $query = "SELECT * FROM participant WHERE session_id = :session_id AND id = :participant_id";
+            $statement = $this->connection->prepare($query);
+            $statement->bindParam(":session_id", $id);
+            $statement->bindParam(":participant_id", $participantId);
+            $statement->execute();
+            $itemCount = $statement->rowCount();
+            if ($itemCount > 0) {
+                $result = $this->database->fetchFirst($statement);
                 return strtoupper(Role::PARTICIPANT);
             }
         }
@@ -247,6 +274,9 @@ class SessionController extends AbstractController
     }
 
     /**
+     * Delete a session.
+     * @param string|null $id The session's ID.
+     * @return string A success or failure message in JSON format.
      * @OA\Delete(
      *   path="/api/session/{id}/",
      *   summary="Delete a session.",
@@ -269,37 +299,38 @@ class SessionController extends AbstractController
     protected function deleteDependencies(string $id)
     {
         $query = "SELECT * FROM participant WHERE session_id = :session_id ";
-        $stmt = $this->connection->prepare($query);
-        $stmt->bindParam(":session_id", $id);
-        $stmt->execute();
+        $statement = $this->connection->prepare($query);
+        $statement->bindParam(":session_id", $id);
+        $statement->execute();
 
-        $result_data = $this->database->fetchAll($stmt);
+        $resultData = $this->database->fetchAll($statement);
         $participant = ParticipantController::getInstance();
-        foreach ($result_data as $result_item) {
-            $participant_id = $result_item["id"];
-            $participant->delete($participant_id);
+        foreach ($resultData as $resultItem) {
+            $participantId = $resultItem["id"];
+            $participant->delete($participantId);
+            // TODO: delete() has no parameters. Adapt the method or the code here?
         }
 
         $query = "SELECT * FROM topic WHERE session_id = :session_id ";
-        $stmt = $this->connection->prepare($query);
-        $stmt->bindParam(":session_id", $id);
-        $stmt->execute();
+        $statement = $this->connection->prepare($query);
+        $statement->bindParam(":session_id", $id);
+        $statement->execute();
 
-        $result_data = $this->database->fetchAll($stmt);
+        $resultData = $this->database->fetchAll($statement);
         $topic = TopicController::getInstance();
-        foreach ($result_data as $result_item) {
-            $topic_id = $result_item["id"];
-            $topic->delete($topic_id);
+        foreach ($resultData as $resultItem) {
+            $topicId = $resultItem["id"];
+            $topic->delete($topicId);
         }
 
         $query = "DELETE FROM resource WHERE session_id = :session_id";
-        $stmt = $this->connection->prepare($query);
-        $stmt->bindParam(":session_id", $id);
-        $stmt->execute();
+        $statement = $this->connection->prepare($query);
+        $statement->bindParam(":session_id", $id);
+        $statement->execute();
 
         $query = "DELETE FROM session_role WHERE session_id = :session_id";
-        $stmt = $this->connection->prepare($query);
-        $stmt->bindParam(":session_id", $id);
-        $stmt->execute();
+        $statement = $this->connection->prepare($query);
+        $statement->bindParam(":session_id", $id);
+        $statement->execute();
     }
 }
