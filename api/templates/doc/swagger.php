3f0ca96171165fa40f04bfa0f57559a2920f4df4
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2021-05-30 23:37:06 +0200

    Updated Swagger UI

--- api/templates/doc/swagger.php
+++ api/templates/doc/swagger.php
@@ -2,39 +2,60 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>API Specification - Swagger UI</title>
-    <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.35.0/swagger-ui.min.css"
-        integrity="sha512-jsql70MmFqKJfWGCXmi3GHPP2q2oi3Ad+6PRQWNeo6df+rxKB07IuBvcCXSrpgKPXaikkQgEQVO2YrtgmSJhUw=="
-        crossorigin="anonymous" />
+    <title>Swagger UI</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.49.0/swagger-ui.css"
+          integrity="sha256-qjZQPPHDerYOSQwo59nugII/sOrHfeIR93CPwMfn5tc=" crossorigin="anonymous">
+    <link rel="icon" type="image/png" href="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.49.0/favicon-32x32.png"
+          sizes="32x32" integrity="sha256-PtYS9B4FDKXnAAytbxy+fn2jn2X8qZwC6Z5lkQVuWDc=" crossorigin="anonymous">
+    <link rel="icon" type="image/png" href="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.49.0/favicon-16x16.png"
+          sizes="16x16" integrity="sha256-ryStYE3Xs7zaj5dauXMHX0ovcKQIeUShL474tjo+B8I=" crossorigin="anonymous">
+    <style>
+        html {
+            box-sizing: border-box;
+            overflow: -moz-scrollbars-vertical;
+            overflow-y: scroll;
+        }
+
+        *,
+        *:before,
+        *:after {
+            box-sizing: inherit;
+        }
+
+        body {
+            margin: 0;
+            background: #fafafa;
+        }
+    </style>
 </head>
+
 <body>
 <div id="swagger-ui"></div>
-<script
-    src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.35.0/swagger-ui-standalone-preset.min.js"
-    integrity="sha512-WI88XrK/8xukiZdnlwlGrcdIyD9qgNXL15LiWbVnq0qpgd/YzRiewFplb5VyRxsbwZf7wRU5BnkCeNP/OV5CEg=="
-    crossorigin="anonymous"></script>
-<script
-    src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.35.0/swagger-ui-bundle.min.js"
-    integrity="sha512-7aNGLo3pjgERnsRoSSRrr8Xy6lX8QeKJG3sh8qAeKDvRCExTvDxG6IPRNrCoY0EZG9B5BzGWV5l0xK9DqSSu+w=="
-    crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.49.0/swagger-ui-bundle.js"
+        integrity="sha256-nC7Ukx9Ilpahz0CL5IusnOX7B8MGUeTQ9jBpV6Zs93A=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.49.0/swagger-ui-standalone-preset.js"
+        integrity="sha256-adIBVcA+py9J70k9hSlHDX/q1r+phR0adACVefz1w14=" crossorigin="anonymous"></script>
 <script>
     window.onload = function () {
+        // Begin Swagger UI call region
         const ui = SwaggerUIBundle({
             spec: <?= $spec ?>,
-        dom_id: '#swagger-ui',
+            dom_id: '#swagger-ui',
             deepLinking: true,
-            supportedSubmitMethods: [],
             presets: [
-            SwaggerUIBundle.presets.apis,
-        ],
+                SwaggerUIBundle.presets.apis,
+                SwaggerUIStandalonePreset
+            ],
             plugins: [
-            SwaggerUIBundle.plugins.DownloadUrl
-        ],
-    })
-        window.ui = ui
-    }
+                SwaggerUIBundle.plugins.DownloadUrl
+            ],
+            layout: "StandaloneLayout"
+        });
+        // End Swagger UI call region
+
+        window.ui = ui;
+    };
 </script>
 </body>
 </html>
