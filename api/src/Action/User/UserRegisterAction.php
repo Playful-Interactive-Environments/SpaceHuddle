94c89fbeed05bcdd2ce249e95e8dfb8269e40458
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2021-06-01 09:27:19 +0200

    Added UserRegisterAction dummy
    Not implemented yet.

---
+++ api/src/Action/User/UserRegisterAction.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Action\User;
+
+use App\Domain\User\Service\UserCreator;
+use App\Responder\Responder;
+use Fig\Http\Message\StatusCodeInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Action for registering a new user.
+ * @package App\Action\User
+ * @OA\Post(
+ *   path="/api/user/register",
+ *   summary="Register a new user",
+ *   tags={"User"},
+ *   @OA\RequestBody(
+ *     @OA\MediaType(
+ *       mediaType="application/json",
+ *       @OA\Schema(required={"username", "password", "passwordConfirmation"},
+ *         @OA\Property(property="username", type="string", example="john.doe"),
+ *         @OA\Property(property="password", type="string", example="secret"),
+ *         @OA\Property(property="passwordConfirmation", type="string", example="secret")
+ *       )
+ *     )
+ *   ),
+ *   @OA\Response(response="201", ref="#/components/responses/201"),
+ *   @OA\Response(response="400", ref="#/components/responses/400"),
+ *   @OA\Response(response="403", ref="#/components/responses/403"),
+ *   @OA\Response(response="422", ref="#/components/responses/422"),
+ *   @OA\Response(response="500", ref="#/components/responses/500")
+ * )
+ */
+final class UserRegisterAction
+{
+    private Responder $responder;
+
+    private UserCreator $userCreator;
+
+    public function __construct(Responder $responder, UserCreator $userCreator)
+    {
+        $this->responder = $responder;
+        $this->userCreator = $userCreator;
+    }
+
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
+    {
+        // Extract the form data from the request body
+        $data = (array)$request->getParsedBody();
+
+        $this->userCreator->createUser($data);
+
+        return $this->responder
+            ->withJson($response, ["id" => 1])
+            ->withStatus(StatusCodeInterface::STATUS_CREATED);
+    }
+}
