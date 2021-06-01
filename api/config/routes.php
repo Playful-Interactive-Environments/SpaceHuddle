5adb428246fffd93bd8e92d0bfaea6fc88ad059a
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2021-06-01 09:30:16 +0200

    Updated routes
    Added GAB routes.

--- api/config/routes.php
+++ api/config/routes.php
@@ -8,21 +8,28 @@ use Tuupola\Middleware\HttpBasicAuthentication;
 
 return function (App $app) {
     // Redirect to Swagger documentation
-    $app->get('/', \App\Action\Home\HomeAction::class)->setName('home');
+    $app->get("/", \App\Action\Home\HomeAction::class)->setName("home");
 
     // Swagger API documentation
-    $app->get('/docs/v1', \App\Action\OpenApi\Version1DocAction::class)->setName('docs');
+    $app->get("/docs/v1", \App\Action\OpenApi\Version1DocAction::class)->setName("docs");
+
+    //$app->get("/user/register", \App\Action\User\UserRegisterAction::class);
 
     // Password protected area
     $app->group(
-        '/api',
+        "/user",
         function (RouteCollectorProxy $app) {
-            $app->get('/users', \App\Action\User\UserFindAction::class);
-            $app->post('/users', \App\Action\User\UserCreateAction::class);
-            $app->get('/users/{user_id}', \App\Action\User\UserReadAction::class);
-            $app->put('/users/{user_id}', \App\Action\User\UserUpdateAction::class);
-            $app->delete('/users/{user_id}', \App\Action\User\UserDeleteAction::class);
-            $app->post('/user/login/', \App\Action\User\UserLoginAction::class);
+            /*$app->get("/users", \App\Action\User\UserFindAction::class);
+            $app->post("/users", \App\Action\User\UserCreateAction::class);
+            $app->get("/users/{user_id}", \App\Action\User\UserReadAction::class);
+            $app->put("/users/{user_id}", \App\Action\User\UserUpdateAction::class);
+            $app->delete("/users/{user_id}", \App\Action\User\UserDeleteAction::class);
+            $app->post("/user/login/", \App\Action\User\UserLoginAction::class);*/
+            $app->post("/login", \App\Action\User\UserLoginAction::class);
+            $app->get("/register", \App\Action\User\UserRegisterAction::class);
+            $app->post("/register", \App\Action\User\UserRegisterAction::class);
+            $app->put("", \App\Action\User\UserChangePasswordAction::class);
+            $app->delete("", \App\Action\User\UserDeleteAction::class);
         }
-    )->add(HttpBasicAuthentication::class);
+    );//->add(HttpBasicAuthentication::class);
 };
