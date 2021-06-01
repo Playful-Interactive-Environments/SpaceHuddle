811eaf14bc1f91c4a6e2f774ffedf524934f96e9
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2021-06-01 09:28:46 +0200

    Updated UserData
    To match with GAB API and database.

--- api/src/Domain/User/Data/UserData.php
+++ api/src/Domain/User/Data/UserData.php
@@ -5,27 +5,24 @@ namespace App\Domain\User\Data;
 use Selective\ArrayReader\ArrayReader;
 
 /**
- * Data Model.
+ * Data Model for User.
+ * @OA\Schema(description="User description")
  */
 final class UserData
 {
-    public ?int $id = null;
+    /**
+     * The user ID.
+     * @var string|null
+     * @OA\Property()
+     */
+    public ?string $id = null;
 
+    /**
+     * The username of the user.
+     * @var string|null
+     * @OA\Property()
+     */
     public ?string $username = null;
-
-    public ?string $password = null;
-
-    public ?string $email = null;
-
-    public ?string $firstName = null;
-
-    public ?string $lastName = null;
-
-    public ?int $userRoleId = null;
-
-    public ?string $locale = null;
-
-    public ?bool $enabled = false;
 
     /**
      * The constructor.
@@ -36,14 +33,7 @@ final class UserData
     {
         $reader = new ArrayReader($data);
 
-        $this->id = $reader->findInt('id');
+        $this->id = $reader->findString("id");
         $this->username = $reader->findString('username');
-        $this->firstName = $reader->findString('first_name');
-        $this->lastName = $reader->findString('last_name');
-        $this->password = $reader->findString('password');
-        $this->email = $reader->findString('email');
-        $this->userRoleId = $reader->findInt('user_role_id');
-        $this->locale = $reader->findString('locale');
-        $this->enabled = $reader->findBool('enabled');
     }
 }
