b5b4e2aaa92cbdf2c93f6896637f6ffbe218c0fd
lucy237 <lucy.schach@gmail.com>
2021-06-04 10:57:57 +0200

    First Draft

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,38 +1,44 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue';
-import Join from '../views/client/Join.vue';
-import ModuleOverview from '@/views/client/Overview.vue';
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Home from "../views/Home.vue";
+import Join from "../views/client/Join.vue";
+import ModuleOverview from "@/views/client/Overview.vue";
+import SessionOverview from "@/views/moderator/SessionOverview.vue";
 
 const routes: Array<RouteRecordRaw> = [
-    {
-        path: '/',
-        name: 'home',
-        component: Home,
-    },
-    {
-        path: '/about',
-        name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import(/* webpackChunkName: "about" */ '../views/About.vue'),
-    },
-    {
-        path: '/join',
-        name: 'join',
-        component: Join,
-    },
-    {
-        path: '/overview',
-        name: 'module-overview',
-        component: ModuleOverview,
-    },
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/join",
+    name: "join",
+    component: Join,
+  },
+  {
+    path: "/overview",
+    name: "module-overview",
+    component: ModuleOverview,
+  },
+  {
+    path: "/session-overview",
+    name: "session-overview",
+    component: SessionOverview,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes,
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
 });
 
 export default router;
---
+++ frontend/src/views/moderator/SessionOverview.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="session-overview container--fullheight">
+    <h1>Your Sessions</h1>
+    <div class="grid-container">
+      <div class="grid-item">
+        <div class="blog-card">
+          <p class="uppercaseOverview">24.08.2020</p>
+          <h3 class="session-title">Marketing Meeting</h3>
+          <p>short description about the goals of this session and other stuff...</p>
+          <p class="uppercaseOverview">topics</p>
+          <p class="uppercaseOverview">modules</p>
+          <h4>XAS54K</h4>
+          <button class="btn btn--mint">Go to session</button>
+        </div>
+      </div>
+      <div class="grid-item">
+        <div class="blog-card">
+          <p class="uppercaseOverview">24.08.2020</p>
+          <h3 class="session-title">Marketing Meeting</h3>
+          <p>short description about the goals of this session and other stuff...</p>
+          <p class="uppercaseOverview">topics</p>
+          <p class="uppercaseOverview">modules</p>
+          <h4>XAS54K</h4>
+          <button class="btn btn--mint">Go to session</button>
+        </div>
+      </div>
+      <div class="grid-item">
+        <div class="blog-card">
+          <p class="uppercaseOverview">24.08.2020</p>
+          <h3 class="session-title">Marketing Meeting</h3>
+          <p>short description about the goals of this session and other stuff...</p>
+          <p class="uppercaseOverview">topics</p>
+          <p class="uppercaseOverview">modules</p>
+          <h4>XAS54K</h4>
+          <button class="btn btn--mint">Go to session</button>
+        </div>
+      </div>
+      <div class="grid-item">
+        <div class="blog-card">
+          <p class="uppercaseOverview">24.08.2020</p>
+          <h3 class="session-title">Marketing Meeting</h3>
+          <p>short description about the goals of this session and other stuff...</p>
+          <p class="uppercaseOverview">topics</p>
+          <p class="uppercaseOverview">modules</p>
+          <h4>XAS54K</h4>
+          <button class="btn btn--mint">Go to session</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+export default class SessionOverview extends Vue {}
+</script>
+
+<style scoped>
+
+.grid-container {
+  display: grid;
+  grid-gap: 50px 50px;
+  grid-template-columns: auto auto auto;
+}
+
+.session-overview {
+  background: #E5E5E5;
+  white: #fff;
+  /* Quick fix für Content Breite */
+  padding: 60px 120px 60px 120px;
+}
+p{
+  font-size: 0.8rem;
+}
+.uppercaseOverview{
+  text-transform: uppercase;
+  color: #aaaaaa;
+}
+
+.blog-card {
+  margin: auto;
+  padding: 15px 30px 15px 30px;
+  display: flex;
+  flex-direction: column;
+  background: #fff;
+  box-shadow: 0.15rem 0.15rem 0.5rem 0.01rem rgba(0, 0, 0, 0.2);
+  border-radius: 1rem;
+  overflow: hidden;
+}
+</style>
+
+
