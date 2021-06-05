926944c0e22604571c006f801607bb3927abcbe0
lucy237 <lucy.schach@gmail.com>
2021-06-05 22:58:12 +0200

    Refactor to Card Component

---
+++ frontend/src/components/shared/atoms/Card.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="blog-card">
+        <p class="uppercaseOverview"><slot name="date">24.08.2020</slot></p>
+        <h3 class="title"><slot name="title"></slot></h3>
+        <p><slot name="description"></slot></p>
+        <p class="uppercaseOverview"><slot name="topics"></slot></p>
+        <p class="uppercaseOverview"><slot name="modules"></slot></p>
+        <slot>My default text</slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { Prop } from 'vue-property-decorator';
+import { Vue } from 'vue-class-component';
+
+export default class Card extends Vue {
+    @Prop({ default: [] }) errors!: string[];
+}
+</script>
+
+<style scoped>
+p {
+    font-size: 0.8rem;
+}
+.blog-card {
+    margin: auto;
+    padding: 15px 30px 15px 30px;
+    background: #fff;
+    box-shadow: 0.15rem 0.15rem 0.5rem 0.01rem rgba(0, 0, 0, 0.2);
+    border-radius: 1rem;
+    overflow: hidden;
+}
+.uppercaseOverview {
+    text-transform: uppercase;
+    color: #aaaaaa;
+    font-size: 0.8rem;
+}
+</style>
--- frontend/src/views/moderator/SessionOverview.vue
+++ frontend/src/views/moderator/SessionOverview.vue
@@ -1,93 +1,82 @@
 <template>
-  <div class="session-overview container--fullheight">
-    <h1>Your Sessions</h1>
-    <div class="grid-container">
-      <div class="grid-item">
-        <div class="blog-card">
-          <p class="uppercaseOverview">24.08.2020</p>
-          <h3 class="session-title">Marketing Meeting</h3>
-          <p>short description about the goals of this session and other stuff...</p>
-          <p class="uppercaseOverview">topics</p>
-          <p class="uppercaseOverview">modules</p>
-          <h4>XAS54K</h4>
-          <button class="btn btn--mint">Go to session</button>
+    <div class="session-overview container--fullheight">
+        <h1>Your Sessions</h1>
+        <div class="grid-container">
+            <div class="grid-item">
+                <card>
+                    <template v-slot:date> 24.08.2020 </template>
+                    <template v-slot:title> Marketing Meeting </template>
+                    <template v-slot:description>
+                        short description about the goals of this session and
+                        other stuff...
+                    </template>
+                    <template v-slot:default>
+                        <h4>XAS54K</h4>
+                        <button class="btn btn--mint">Go to session</button>
+                    </template>
+                    <template v-slot:topics>topics</template>
+                    <template v-slot:modules>modules</template>
+                </card>
+            </div>
+            <div class="grid-item">
+                <card>
+                    <template v-slot:date> 24.08.2020 </template>
+                    <template v-slot:title> Marketing Meeting </template>
+                    <template v-slot:description>
+                        short description about the goals of this session and
+                        other stuff...
+                    </template>
+                    <template v-slot:default>
+                        <h4>XAS54K</h4>
+                        <button class="btn btn--mint">Go to session</button>
+                    </template>
+                    <template v-slot:topics>topics</template>
+                    <template v-slot:modules>modules</template>
+                </card>
+            </div>
+            <div class="grid-item">
+                <card>
+                    <template v-slot:date> 24.08.2020 </template>
+                    <template v-slot:title> Marketing Meeting </template>
+                    <template v-slot:description>
+                        short description about the goals of this session and
+                        other stuff...
+                    </template>
+                    <template v-slot:default>
+                        <h4>XAS54K</h4>
+                        <button class="btn btn--mint">Go to session</button>
+                    </template>
+                    <template v-slot:topics>topics</template>
+                    <template v-slot:modules>modules</template>
+                </card>
+            </div>
         </div>
-      </div>
-      <div class="grid-item">
-        <div class="blog-card">
-          <p class="uppercaseOverview">24.08.2020</p>
-          <h3 class="session-title">Marketing Meeting</h3>
-          <p>short description about the goals of this session and other stuff...</p>
-          <p class="uppercaseOverview">topics</p>
-          <p class="uppercaseOverview">modules</p>
-          <h4>XAS54K</h4>
-          <button class="btn btn--mint">Go to session</button>
-        </div>
-      </div>
-      <div class="grid-item">
-        <div class="blog-card">
-          <p class="uppercaseOverview">24.08.2020</p>
-          <h3 class="session-title">Marketing Meeting</h3>
-          <p>short description about the goals of this session and other stuff...</p>
-          <p class="uppercaseOverview">topics</p>
-          <p class="uppercaseOverview">modules</p>
-          <h4>XAS54K</h4>
-          <button class="btn btn--mint">Go to session</button>
-        </div>
-      </div>
-      <div class="grid-item">
-        <div class="blog-card">
-          <p class="uppercaseOverview">24.08.2020</p>
-          <h3 class="session-title">Marketing Meeting</h3>
-          <p>short description about the goals of this session and other stuff...</p>
-          <p class="uppercaseOverview">topics</p>
-          <p class="uppercaseOverview">modules</p>
-          <h4>XAS54K</h4>
-          <button class="btn btn--mint">Go to session</button>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import Card from '../../components/shared/atoms/Card.vue';
 
+@Options({
+    components: {
+        Card,
+    },
+})
 export default class SessionOverview extends Vue {}
 </script>
 
 <style scoped>
-
 .grid-container {
-  display: grid;
-  grid-gap: 50px 50px;
-  grid-template-columns: auto auto auto;
+    display: grid;
+    grid-gap: 50px 50px;
+    grid-template-columns: auto auto auto;
 }
-
 .session-overview {
-  background: #E5E5E5;
-  white: #fff;
-  /* Quick fix für Content Breite */
-  padding: 60px 120px 60px 120px;
+    background: #e5e5e5;
+    padding: 60px 120px 60px 120px;
 }
-p{
-  font-size: 0.8rem;
-}
-.uppercaseOverview{
-  text-transform: uppercase;
-  color: #aaaaaa;
-}
-
-.blog-card {
-  margin: auto;
-  padding: 15px 30px 15px 30px;
-  display: flex;
-  flex-direction: column;
-  background: #fff;
-  box-shadow: 0.15rem 0.15rem 0.5rem 0.01rem rgba(0, 0, 0, 0.2);
-  border-radius: 1rem;
-  overflow: hidden;
+p {
+    font-size: 0.8rem;
 }
 </style>
-
-
