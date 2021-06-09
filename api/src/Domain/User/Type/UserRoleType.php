1b3bd13ba2e5c63359d1325702f6e34cb462d982
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2021-06-10 01:34:56 +0200

    Comments for UserRoleType

--- api/src/Domain/User/Type/UserRoleType.php
+++ api/src/Domain/User/Type/UserRoleType.php
@@ -5,7 +5,7 @@ namespace App\Domain\User\Type;
 /**
  * Permission roles for the session.
  * @OA\Schema(
- *   description="permission roles for the session",
+ *   description="Permission roles for the session",
  *   type="string",
  *   enum={"MODERATOR", "FACILITATOR", "PARTICIPANT", "UNKNOWN", "PARTICIPANT_INACTIVE"},
  *   example="MODERATOR"
@@ -13,9 +13,18 @@ namespace App\Domain\User\Type;
  */
 class UserRoleType
 {
+    /** @var string */
     public const MODERATOR = "moderator";
+
+    /** @var string */
     public const FACILITATOR = "facilitator";
+
+    /** @var string */
     public const PARTICIPANT = "participant";
+
+    /** @var string */
     public const UNKNOWN = "unknown";
+
+    /** @var string */
     public const PARTICIPANT_INACTIVE = "participant_inactive";
 }
