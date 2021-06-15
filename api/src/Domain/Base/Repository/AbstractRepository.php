40721f4ffb0dfc2d73a94946502fda1c8198292e
Andrea Aschauer <andrea.aschauer@fh-hagenberg.at>
2021-06-15 11:33:10 +0200

    #1 add authorisation role trait

--- api/src/Domain/Base/Repository/AbstractRepository.php
+++ api/src/Domain/Base/Repository/AbstractRepository.php
@@ -2,9 +2,7 @@
 
 namespace App\Domain\Base\Repository;
 
-use App\Data\AuthorisationData;
 use App\Domain\Base\Data\AbstractData;
-use App\Domain\Session\Type\SessionRoleType;
 use App\Factory\QueryFactory;
 use DomainException;
 
@@ -16,6 +14,7 @@ abstract class AbstractRepository
 {
     use GenericTrait;
     use MagicPropertiesTrait;
+    use AuthorisationRoleTrait;
 
     protected QueryFactory $queryFactory;
 
@@ -36,44 +35,6 @@ abstract class AbstractRepository
     ) {
         $this->queryFactory = $queryFactory;
         $this->setGenerics($queryFactory, $entityName, $resultClass, $parentIdName, $parentRepository);
-    }
-
-    /**
-     * Checks the access role via which the logged-in user may access the entry with the specified primary key.
-     * @param AuthorisationData $authorisation Authorisation token data.
-     * @param string|null $id Primary key to be checked.
-     * @return string|null Role with which the user is authorised to access the entry.
-     * @throws GenericException
-     */
-    public function getAuthorisationRole(AuthorisationData $authorisation, ?string $id): ?string
-    {
-        if (is_null($id)) {
-            return SessionRoleType::mapAuthorisationType($authorisation->type);
-        } else {
-            $query = $this->queryFactory->newSelect($this->getEntityName());
-            $query->select(["*"])
-                ->andWhere(["id" => $id]);
-            $statement = $query->execute();
-            $itemCount = $statement->rowCount();
-            if ($itemCount > 0) {
-                $parentId = $statement->fetch("assoc")[$this->getParentIdName()];
-                return $this->getParentRepository()->getAuthorisationRole($authorisation, $parentId);
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Checks whether the user is authorised to read the entry with the specified primary key.
-     * @param AuthorisationData $authorisation Authorisation token data.
-     * @param string|null $id Primary key to be checked.
-     * @return string|null Role with which the user is authorised to access the entry.
-     * @throws GenericException
-     */
-    public function getAuthorisationReadRole(AuthorisationData $authorisation, ?string $id): ?string
-    {
-        return $this->getAuthorisationRole($authorisation, $id);
     }
 
     /**
---
+++ api/src/Domain/Base/Repository/AuthorisationRoleTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Domain\Base\Repository;
+
+use App\Data\AuthorisationData;
+use App\Domain\Session\Type\SessionRoleType;
+
+trait AuthorisationRoleTrait
+{
+    /**
+     * Checks the access role via which the logged-in user may access the entry with the specified primary key.
+     * @param AuthorisationData $authorisation Authorisation token data.
+     * @param string|null $id Primary key to be checked.
+     * @return string|null Role with which the user is authorised to access the entry.
+     * @throws GenericException
+     */
+    public function getAuthorisationRole(AuthorisationData $authorisation, ?string $id): ?string
+    {
+        if (is_null($id)) {
+            return SessionRoleType::mapAuthorisationType($authorisation->type);
+        } else {
+            $query = $this->queryFactory->newSelect($this->getEntityName());
+            $query->select(["*"])
+                ->andWhere(["id" => $id]);
+            $statement = $query->execute();
+            $itemCount = $statement->rowCount();
+            if ($itemCount > 0) {
+                $parentId = $statement->fetch("assoc")[$this->getParentIdName()];
+                return $this->getParentRepository()->getAuthorisationRole($authorisation, $parentId);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Checks whether the user is authorised to read the entry with the specified primary key.
+     * @param AuthorisationData $authorisation Authorisation token data.
+     * @param string|null $id Primary key to be checked.
+     * @return string|null Role with which the user is authorised to access the entry.
+     * @throws GenericException
+     */
+    public function getAuthorisationReadRole(AuthorisationData $authorisation, ?string $id): ?string
+    {
+        return $this->getAuthorisationRole($authorisation, $id);
+    }
+}
