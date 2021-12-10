71462ec551583cd3ceb7ac9870667b08c7474cc5
Andrea Aschauer <andrea.aschauer@fh-hagenberg.at>
2021-12-10 09:37:41 +0100

    #frontend: add max count + count sort for view details

--- frontend/src/services/view-service.ts
+++ frontend/src/services/view-service.ts
@@ -18,22 +18,43 @@ interface InputData {
   order: string;
 }
 
+const pushQueryParameter = (
+  queryParameter: string,
+  parameterString: string
+): string => {
+  if (queryParameter.length > 0) return `${queryParameter}&${parameterString}`;
+  else return `?${parameterString}`;
+};
+
 export const getDetail = async (
   type: string,
   typeId: string,
   orderType: string | null = null,
   refId: string | null = null,
   filter: string[] | null = null,
+  count: number | null,
+  countOrderType: string | null = null,
+  countRefId: string | null = null,
   authHeaderType = EndpointAuthorisationType.MODERATOR
 ): Promise<Idea[]> => {
   let queryParameter = '';
   if (orderType) queryParameter = `?order=${orderType}`;
   if (refId && orderType) queryParameter = `${queryParameter}&refId=${refId}`;
   if (filter && filter.length > 0) {
-    const filterString = `filter=${JSON.stringify(filter)}`;
-    if (queryParameter.length > 0)
-      queryParameter = `${queryParameter}&${filterString}`;
-    else queryParameter = `?${filterString}`;
+    const parameterString = `filter=${JSON.stringify(filter)}`;
+    queryParameter = pushQueryParameter(queryParameter, parameterString);
+  }
+  if (count) {
+    const parameterString = `count=${count}`;
+    queryParameter = pushQueryParameter(queryParameter, parameterString);
+  }
+  if (countOrderType) {
+    const parameterString = `countOrderType=${countOrderType}`;
+    queryParameter = pushQueryParameter(queryParameter, parameterString);
+  }
+  if (countRefId) {
+    const parameterString = `countRefId=${countRefId}`;
+    queryParameter = pushQueryParameter(queryParameter, parameterString);
   }
   return await apiExecuteGetHandled<Idea[]>(
     `/${EndpointType.VIEW}/${type}/${typeId}/${queryParameter}`,
@@ -48,15 +69,32 @@ export const getIdeas = async (
   refId: string | null = null,
   authHeaderType = EndpointAuthorisationType.MODERATOR
 ): Promise<Idea[]> => {
+  if (orderType) {
+    const constOrderParts = orderType.split('&refId=');
+    if (constOrderParts.length > 1) {
+      refId = constOrderParts[1];
+      orderType = constOrderParts[0];
+    }
+  }
   const result: Idea[] = [];
   for (const index in input) {
     const item = input[index];
+    const orderParts = item.order.split('&refId=');
+    let countRefId: string | null = null;
+    let countOrderType: string | null = item.order;
+    if (orderParts.length > 1) {
+      countOrderType = orderParts[0];
+      countRefId = orderParts[1];
+    }
     await getDetail(
       item.view.type,
       item.view.id,
-      orderType ? orderType : item.order,
-      orderType ? refId : null,
+      orderType ? orderType : countOrderType,
+      orderType ? refId : countRefId,
       item.filter,
+      item.maxCount,
+      countOrderType,
+      countRefId,
       authHeaderType
     ).then((inputResult) => {
       if (item.maxCount) inputResult = inputResult.slice(0, item.maxCount);
