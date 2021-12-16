03e71d7abee9fead33d3899d72cac41f2c296c6a
Andrea Aschauer <andrea.aschauer@fh-hagenberg.at>
2021-12-16 14:56:42 +0100

    #backend: send mail

--- api/src/Domain/Base/Service/MailTrait.php
+++ api/src/Domain/Base/Service/MailTrait.php
@@ -26,10 +26,6 @@ trait MailTrait
         // To send HTML mail, the Content-type header must be set
         $headers  = 'MIME-Version: 1.0' . "\r\n";
         $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-        // Create email headers
-        $headers .= 'From: '.$from."\r\n".
-            'Reply-To: '.$from."\r\n";
         return $headers;
     }
 
