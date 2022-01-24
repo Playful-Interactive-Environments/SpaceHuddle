edbfe482672234110b3bf98cdf14169eefbbd1e9
Andrea Aschauer <andrea.aschauer@fh-hagenberg.at>
2022-01-24 18:07:04 +0100

    #frontend: implement first game module

--- frontend/src/modules/brainstorming/game/output/Participant.vue
+++ frontend/src/modules/brainstorming/game/output/Participant.vue
@@ -1,18 +1,18 @@
 <template>
-  <ParticipantModuleDefaultContainer :task-id="taskId" :module="moduleName">
-    <template v-slot:planet>
-      <img
-        src="@/assets/illustrations/planets/brainstorming.png"
-        alt="planet"
-        class="module-container__planet"
-      />
-    </template>
-    <div class="centered flex-column fill">
-      <span v-if="randomIdea">{{ randomIdea.keywords }}</span>
-      <br />
-      <span v-if="randomIdea">{{ randomIdea.description }}</span>
+  <div class="canvas-container">
+    <canvas
+      ref="canvas"
+      id="canvas"
+      width="560"
+      height="360"
+      v-on:mousemove="draw"
+    />
+    <div class="text-animation" v-if="randomIdea" ref="textAnimation">
+      <span v-for="(char, index) in randomIdea.keywords" :key="index">
+        {{ char }}
+      </span>
     </div>
-  </ParticipantModuleDefaultContainer>
+  </div>
 </template>
 
 <script lang="ts">
@@ -24,6 +24,17 @@ import * as moduleService from '@/services/module-service';
 import { Idea } from '@/types/api/Idea.ts';
 import { Module } from '@/types/api/Module';
 import EndpointAuthorisationType from '@/types/enum/EndpointAuthorisationType';
+import { CanvasBodies } from '@/types/ui/CanvasBodies';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const orientations: string[][] = [
+  ['landscape left', 'landscape right'], // device x axis points up/down
+  ['portrait', 'portrait upside down'], // device y axis points up/down
+  ['display up', 'display down'], // device z axis points up/down
+];
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const rad: number = Math.PI / 180;
 
 @Options({
   components: {
@@ -36,10 +47,52 @@ export default class Participant extends Vue {
   @Prop() readonly taskId!: string;
   @Prop() readonly moduleId!: string;
   module: Module | null = null;
-  readonly intervalTime = 10000;
+  readonly intervalTime = 100000;
   interval!: any;
 
   randomIdea: Idea | null = null;
+
+  vueCanvas!: CanvasRenderingContext2D;
+  readonly drawingIntervalTime = 100;
+  drawingInterval!: any;
+  mousePos: any = null;
+  bodies!: CanvasBodies;
+  shakeEvent: any;
+
+  changeText(): void {
+    if (this.randomIdea && this.$refs.textAnimation) {
+      this.bodies.clearTexts();
+      this.bodies.startAnimation();
+      const textAnimation = this.$refs.textAnimation as any;
+      const rectAnimation = textAnimation.getBoundingClientRect();
+      textAnimation.childNodes.forEach((span) => {
+        if (span.tagName === 'SPAN') {
+          const rect = span.getBoundingClientRect();
+          this.bodies.addText(
+            span.innerHTML,
+            (rect.left + rect.right) / 2 - rectAnimation.x,
+            (rect.top + rect.bottom) / 2 - rectAnimation.y,
+            70,
+            '#FFFFFFFF'
+          );
+        }
+      });
+    }
+  }
+
+  get vueCanvasWidth(): number {
+    if (this.$refs.canvas) {
+      return (this.$refs.canvas as any).width;
+    }
+    return 0;
+  }
+
+  get vueCanvasHeight(): number {
+    if (this.$refs.canvas) {
+      return (this.$refs.canvas as any).height;
+    }
+    return 0;
+  }
 
   get moduleName(): string {
     if (this.module) return this.module.name;
@@ -47,8 +100,99 @@ export default class Participant extends Vue {
   }
 
   async mounted(): Promise<void> {
+    if (this.$refs.canvas) {
+      (this.$refs.canvas as any).width = (
+        this.$refs.canvas as any
+      ).parentElement.offsetWidth;
+      (this.$refs.canvas as any).height = (
+        this.$refs.canvas as any
+      ).parentElement.offsetHeight;
+      this.vueCanvas = (this.$refs.canvas as any).getContext('2d');
+    }
+    window.addEventListener('deviceorientation', this.onOrientationChange);
     await this.getTaskIdeas();
     this.startInterval();
+
+    this.setupPhysics();
+    this.drawingInterval = setInterval(() => {
+      this.update_drawing();
+    }, this.drawingIntervalTime);
+    this.setupShaking();
+  }
+
+  setupShaking(): void {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const Shake = require('shake.js');
+    this.shakeEvent = new Shake({ threshold: 15 });
+    this.shakeEvent.start();
+    window.addEventListener('shake', this.isShaking, false);
+  }
+
+  isShaking(): void {
+    this.getTaskIdeas();
+    //this.changeText();
+  }
+
+  setupPhysics(): void {
+    this.bodies = new CanvasBodies(
+      this.vueCanvas,
+      this.vueCanvasWidth,
+      this.vueCanvasHeight
+    );
+    const letterCount = 26;
+    for (let i = 0; i < 100; i++) {
+      const r = Math.floor(Math.random() * 15 + 15);
+      const x = Math.floor(Math.random() * (this.vueCanvasWidth - r * 2) + r);
+      const y = Math.floor(Math.random() * (this.vueCanvasHeight - r * 2) + r);
+      const a = 'A';
+      const text = String.fromCharCode(a.charCodeAt(0) + (i % letterCount));
+      this.bodies.addCircle(x, y, r, { text: text, gradientSize: r });
+    }
+    const borderSize = 1;
+    this.bodies.addRect(
+      this.vueCanvasWidth / 2,
+      this.vueCanvasHeight - borderSize / 2,
+      this.vueCanvasWidth,
+      borderSize,
+      { isStatic: true }
+    );
+    this.bodies.addRect(
+      this.vueCanvasWidth / 2,
+      borderSize / 2,
+      this.vueCanvasWidth,
+      borderSize,
+      { isStatic: true }
+    );
+    this.bodies.addRect(
+      borderSize / 2,
+      this.vueCanvasHeight / 2,
+      borderSize,
+      this.vueCanvasHeight,
+      { isStatic: true }
+    );
+    this.bodies.addRect(
+      this.vueCanvasWidth - borderSize / 2,
+      this.vueCanvasHeight / 2,
+      borderSize,
+      this.vueCanvasHeight,
+      { isStatic: true }
+    );
+  }
+
+  onOrientationChange(ev: DeviceOrientationEvent): void {
+    if (ev.alpha && ev.beta && ev.gamma) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const Quaternion = require('quaternion');
+      const q = Quaternion.fromEuler(
+        ev.alpha * rad,
+        ev.beta * rad,
+        ev.gamma * rad,
+        'ZXY'
+      );
+      // transform an upward-pointing vector to device coordinates
+      const vec = q.conjugate().rotateVector([0, 0, 1]);
+      this.bodies.setGravity(vec[0], vec[1]);
+    }
   }
 
   @Watch('moduleId', { immediate: true })
@@ -72,6 +216,8 @@ export default class Participant extends Vue {
 
   unmounted(): void {
     clearInterval(this.interval);
+    clearInterval(this.drawingInterval);
+    this.shakeEvent.stop();
   }
 
   async getTaskIdeas(): Promise<void> {
@@ -86,8 +232,52 @@ export default class Participant extends Vue {
         const randomIndex = Math.floor(Math.random() * queryResult.length);
         this.randomIdea = queryResult[randomIndex];
       });
+
+    this.changeText();
+  }
+
+  draw(e: any): void {
+    this.mousePos = [e.offsetX, e.offsetY];
+  }
+
+  async update_drawing(): Promise<void> {
+    this.bodies.show();
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.canvas-container {
+  color: #fff;
+  background: var(--color-darkblue);
+  background-image: url('~@/assets/illustrations/background.png');
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+  width: 100%;
+  position: relative;
+}
+
+.text-animation {
+  position: absolute;
+  height: 100vh;
+  width: 100%;
+  top: 0;
+  z-index: -1;
+  text-align: center;
+  vertical-align: middle;
+  line-height: 100vh;
+  font-family: Arial, sans-serif;
+  font-size: 70px;
+}
+
+html,
+body {
+  height: 100%;
+}
+
+body {
+  overflow: hidden;
+}
+</style>
---
+++ frontend/src/types/ui/CanvasBodies.ts
@@ -0,0 +1,348 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const Matter = require('@/../node_modules/matter-js/build/matter.js');
+
+// module aliases
+const Engine = Matter.Engine,
+  Runner = Matter.Runner,
+  Bodies = Matter.Bodies,
+  Body = Matter.Body,
+  Composite = Matter.Composite;
+
+const engine = Engine.create();
+const runner = Runner.create();
+Runner.run(runner, engine);
+
+export class CanvasBodies {
+  ctx!: CanvasRenderingContext2D;
+  canvasWidth = 0;
+  canvasHeight = 0;
+  bodies: { [key: string]: any }[] = [];
+  texts: {
+    text: string;
+    x: number;
+    y: number;
+    size: number;
+    color: string;
+    angle: number;
+    animation: {
+      startX: number;
+      startY: number;
+      startSize: number;
+      startAngle: number;
+    };
+  }[] = [];
+  animationTimeline: {
+    fadeAnimationLength: number;
+    animationFrame: number;
+    fadeInAnimationStart: number;
+    fadeOutAnimationStart: number;
+    startOpacity: number;
+    targetOpacity: number;
+  } = {
+    fadeAnimationLength: 10,
+    animationFrame: -1,
+    fadeInAnimationStart: 0,
+    fadeOutAnimationStart: 80,
+    startOpacity: 0,
+    targetOpacity: 255,
+  };
+
+  constructor(
+    ctx: CanvasRenderingContext2D,
+    canvasWidth: number,
+    canvasHeight: number
+  ) {
+    this.ctx = ctx;
+    this.canvasWidth = canvasWidth;
+    this.canvasHeight = canvasHeight;
+    this.clearBodies();
+  }
+
+  readonly defaultGravityScale = 0.00005;
+  setGravity(x: number, y: number): void {
+    engine.gravity = {
+      x: x,
+      y: y,
+      scale: this.defaultGravityScale,
+    };
+  }
+
+  startAnimation(): void {
+    this.animationTimeline.animationFrame = 0;
+    const force = engine.gravity.scale === this.defaultGravityScale ? 10 : 20;
+    engine.world.bodies.forEach((body, index) => {
+      const options = this.bodies[index];
+      if (options.text) {
+        Body.setVelocity(body, {
+          x: -engine.gravity.x * force,
+          y: -engine.gravity.y * force,
+        });
+      }
+    });
+  }
+
+  clearBodies(): void {
+    Composite.clear(engine.world);
+    this.bodies = [];
+  }
+
+  clearCanvas(): void {
+    this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+  }
+
+  addRect(
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    options: { [key: string]: any } = {}
+  ): void {
+    const body = Bodies.rectangle(x, y, width, height, options);
+    Composite.add(engine.world, body);
+    this.bodies.push(options);
+  }
+
+  addCircle(
+    x: number,
+    y: number,
+    radius: number,
+    options: { [key: string]: any } = {}
+  ): void {
+    const body = Bodies.circle(x, y, radius, options);
+    Composite.add(engine.world, body);
+    this.bodies.push(options);
+  }
+
+  addText(
+    text: string,
+    x: number,
+    y: number,
+    size = 24,
+    color = '#FFFFFFFF',
+    angle = 0
+  ): void {
+    const animation = {
+      startX: Math.floor(Math.random() * this.canvasWidth),
+      startY: Math.floor(Math.random() * this.canvasHeight),
+      startSize: size / 3,
+      startOpacity: 0,
+      targetOpacity: 255,
+      startAngle: angle,
+      animationLength: 15,
+      animationFrame: 0,
+      fadeOutAnimationStart: 80,
+    };
+    this.texts.push({
+      text: text,
+      x: x,
+      y: y,
+      size: size,
+      color: color,
+      angle: angle,
+      animation: animation,
+    });
+  }
+
+  clearTexts(): void {
+    this.texts = [];
+  }
+
+  private fadeInAnimationIsRunning(): boolean {
+    return (
+      this.animationTimeline.animationFrame >=
+        this.animationTimeline.fadeInAnimationStart &&
+      this.animationTimeline.animationFrame <=
+        this.animationTimeline.fadeInAnimationStart +
+          this.animationTimeline.fadeAnimationLength
+    );
+  }
+
+  private fadeOutAnimationIsRunning(): boolean {
+    return (
+      this.animationTimeline.animationFrame >=
+        this.animationTimeline.fadeOutAnimationStart &&
+      this.animationTimeline.animationFrame <=
+        this.animationTimeline.fadeOutAnimationStart +
+          this.animationTimeline.fadeAnimationLength
+    );
+  }
+
+  private animationTextIsActive(): boolean {
+    return (
+      this.animationTimeline.animationFrame >=
+        this.animationTimeline.fadeInAnimationStart &&
+      this.animationTimeline.animationFrame <=
+        this.animationTimeline.fadeOutAnimationStart +
+          this.animationTimeline.fadeAnimationLength
+    );
+  }
+
+  private interpolationFrame(): number {
+    if (this.fadeInAnimationIsRunning())
+      return this.animationTimeline.animationFrame;
+    if (this.fadeOutAnimationIsRunning())
+      return (
+        this.animationTimeline.fadeAnimationLength -
+        this.animationTimeline.animationFrame +
+        this.animationTimeline.fadeOutAnimationStart
+      );
+    return -1;
+  }
+
+  completeAnimation(): void {
+    runner.enabled = true;
+
+    engine.world.bodies.forEach((body, index) => {
+      const options = this.bodies[index];
+      if (options.text) {
+        Body.setVelocity(body, { x: 0, y: 0 });
+      }
+    });
+  }
+
+  showBodies(opacity: number): void {
+    let gradOpacity = Math.floor(opacity / 2).toString(16);
+    if (gradOpacity.length === 1) gradOpacity = `0${gradOpacity}`;
+    let hexOpacity = opacity.toString(16);
+    if (hexOpacity.length === 1) hexOpacity = `0${hexOpacity}`;
+    engine.world.bodies.forEach((body, index) => {
+      const options = this.bodies[index];
+      this.ctx.beginPath();
+      const start = body.vertices[0];
+      this.ctx.moveTo(start.x, start.y);
+      for (let i = 1; i < body.vertices.length; i += 1) {
+        const to = body.vertices[i];
+        this.ctx.lineTo(to.x, to.y);
+      }
+      this.ctx.closePath();
+
+      if (options.gradientSize) {
+        // Create gradient
+        const grd = this.ctx.createRadialGradient(
+          body.position.x,
+          body.position.y,
+          1,
+          body.position.x,
+          body.position.y,
+          options.gradientSize
+        );
+        grd.addColorStop(0, `#FFFFFF${gradOpacity}`);
+        grd.addColorStop(1, '#FFFFFF00');
+
+        this.ctx.fillStyle = grd;
+      } else {
+        this.ctx.fillStyle = `#FFFFFF${hexOpacity}`;
+      }
+
+      this.ctx.fill();
+      if (options.text) {
+        this.showText(
+          options.text,
+          body.position.x,
+          body.position.y,
+          options.gradientSize,
+          `#27133B${hexOpacity}`,
+          body.angle
+        );
+      }
+    });
+  }
+
+  show(): void {
+    const interpolate = (
+      start: number,
+      end: number,
+      step: number,
+      stepCount: number
+    ): number => {
+      return start + (step / stepCount) * (end - start);
+    };
+    const interpolationFrame = this.interpolationFrame();
+
+    this.clearCanvas();
+    if (!this.animationTextIsActive()) this.showBodies(255);
+    else {
+      if (
+        this.fadeOutAnimationIsRunning() &&
+        interpolationFrame === this.animationTimeline.fadeAnimationLength
+      ) {
+        this.completeAnimation();
+      }
+      let opacity = interpolate(
+        this.animationTimeline.startOpacity,
+        this.animationTimeline.targetOpacity,
+        interpolationFrame,
+        this.animationTimeline.fadeAnimationLength
+      );
+      if (opacity < 0 || opacity > 255) {
+        opacity = 0;
+      }
+      opacity = Math.floor(opacity);
+      if (this.fadeInAnimationIsRunning() || this.fadeOutAnimationIsRunning()) {
+        this.showBodies(255 - opacity);
+        let hexOpacity = opacity.toString(16);
+        if (hexOpacity.length === 1) hexOpacity = `0${hexOpacity}`;
+        this.texts.forEach((text) => {
+          this.showText(
+            text.text,
+            interpolate(
+              text.animation.startX,
+              text.x,
+              interpolationFrame,
+              this.animationTimeline.fadeAnimationLength
+            ),
+            interpolate(
+              text.animation.startY,
+              text.y,
+              interpolationFrame,
+              this.animationTimeline.fadeAnimationLength
+            ),
+            interpolate(
+              text.animation.startSize,
+              text.size,
+              interpolationFrame,
+              this.animationTimeline.fadeAnimationLength
+            ),
+            `${text.color.substr(0, 7)}${hexOpacity}`,
+            text.angle
+          );
+        });
+      } else {
+        if (runner.enabled) runner.enabled = false;
+        this.texts.forEach((text) => {
+          this.showText(
+            text.text,
+            text.x,
+            text.y,
+            text.size,
+            text.color,
+            text.angle
+          );
+        });
+      }
+    }
+    if (this.animationTimeline.animationFrame >= 0)
+      this.animationTimeline.animationFrame++;
+  }
+
+  showText(
+    text: string,
+    x: number,
+    y: number,
+    size = 24,
+    color = '#27133BFF',
+    angle = 0
+  ): void {
+    this.ctx.font = `${size}px Arial`;
+    this.ctx.textAlign = 'center';
+    this.ctx.textBaseline = 'middle';
+    const textPosX = x;
+    const textPosY = y;
+    this.ctx.translate(textPosX, textPosY);
+    this.ctx.rotate(angle);
+    this.ctx.fillStyle = color;
+    this.ctx.fillText(text, 0, 0);
+    this.ctx.rotate(-angle);
+    this.ctx.translate(-textPosX, -textPosY);
+  }
+}
