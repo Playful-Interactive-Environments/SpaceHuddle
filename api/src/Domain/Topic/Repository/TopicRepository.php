63a955d598b9d96c8e41c1c34c8ad3d068b562d6
Andrea Aschauer <andrea.aschauer@fh-hagenberg.at>
2022-02-11 15:22:04 +0100

    #backend: export data

--- api/src/Domain/Topic/Repository/TopicRepository.php
+++ api/src/Domain/Topic/Repository/TopicRepository.php
@@ -255,7 +255,7 @@ class TopicRepository implements RepositoryInterface
             $writer = new Xlsx($spreadsheet);
             ob_end_clean();
             $writer->save($url);
-            exit();
+            die;
         }
         if ($url) {
             $exportPath = $_SERVER["REQUEST_SCHEME"] . '://' . $_SERVER["HTTP_HOST"] .
