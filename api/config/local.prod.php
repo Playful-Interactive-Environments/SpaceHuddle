51d8a4caccd6feecdf1144e9694f5a629e24a5bf
Andrea Aschauer <andrea.aschauer@fh-hagenberg.at>
2022-02-15 15:19:21 +0100

    #backend: show server errors

--- api/config/local.prod.php
+++ api/config/local.prod.php
@@ -10,3 +10,5 @@ $settings["db"]["database"] = "spacehuddle";
 
 // Application
 $settings["application"]["baseUrl"] = "https://spacehuddle.io";
+
+$settings["error"]["display_error_details"] = true;
