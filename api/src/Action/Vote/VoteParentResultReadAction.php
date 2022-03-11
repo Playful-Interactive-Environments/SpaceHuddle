8a95a6afb17491a2ee439b223a158f10d0bef020
Andrea Aschauer <andrea.aschauer@fh-hagenberg.at>
2022-03-11 14:10:25 +0100

    #backend: read the parent result of the voting

---
+++ api/src/Action/Vote/VoteParentResultReadAction.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Action\Vote;
+
+use App\Action\Base\ActionTrait;
+use App\Domain\Vote\Service\VoteParentResultReader;
+use App\Responder\Responder;
+use Fig\Http\Message\StatusCodeInterface;
+
+/**
+ * Action for reading the result of the voting for the specified parent idea.
+ *
+ * @OA\Get(
+ *   path="/hierarchy/{taskId}/vote_result/",
+ *   summary="Returns the result of the voting for the specified parent idea.",
+ *   tags={"Vote"},
+ *   @OA\Parameter(in="path", name="parentId", description="ID of the parent idea", required=true),
+ *   @OA\Response(response="200", description="Success",
+ *     @OA\MediaType(
+ *         mediaType="application/json",
+ *         @OA\Schema(type="array", @OA\Items(ref="#/components/schemas/VoteResultData")),
+ *     )
+ *   ),
+ *   @OA\Response(response="404", description="Not Found"),
+ *   security={{"api_key": {}}, {"bearerAuth": {}}}
+ * )
+ */
+class VoteParentResultReadAction
+{
+    use ActionTrait;
+    protected VoteParentResultReader $service;
+
+    /**
+     * The constructor.
+     *
+     * @param Responder $responder The responder
+     * @param VoteParentResultReader $service The service
+     */
+    public function __construct(Responder $responder, VoteParentResultReader $service)
+    {
+        $this->setUp($responder);
+        $this->service = $service;
+        $this->successStatusCode = StatusCodeInterface::STATUS_OK;
+    }
+}
---
+++ api/src/Domain/Vote/Service/VoteParentResultReader.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Domain\Vote\Service;
+
+use App\Domain\Base\Service\ServiceReaderTrait;
+
+/**
+ * Service to read result of the voting for one parent idea.
+ */
+class VoteParentResultReader
+{
+    use ServiceReaderTrait;
+    use VoteServiceTrait;
+
+    /**
+     * Validates whether the transferred data is suitable for the service.
+     * @param array $data Data to be verified.
+     * @return void
+     */
+    protected function serviceValidation(array $data): void
+    {
+        $this->validator->validateRead($data);
+    }
+
+    /**
+     * Executes the repository instructions assigned to the service.
+     *
+     * @param array $data Input data from the request.
+     *
+     * @return array|object|null Repository answer.
+     */
+    protected function serviceExecution(
+        array $data
+    ): array|object|null {
+        if (array_key_exists("taskId", $data)) {
+            $taskId = $data["taskId"];
+            // Fetch data from the database
+            return $this->repository->getParentResult($taskId);
+        }
+        return null;
+    }
+}
