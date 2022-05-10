e75bf477e215b1d2923156fa00495985e5631257
Paul Huemer <paul.huemer01@gmail.com>
2022-05-10 14:50:29 +0200

    #frontend: Quiz Moderator Results / Client

--- frontend/src/modules/information/quiz/output/Participant.vue
+++ frontend/src/modules/information/quiz/output/Participant.vue
@@ -1,5 +1,23 @@
 <template>
-  <ParticipantModuleDefaultContainer :task-id="taskId" :module="moduleName">
+  <div id="loadingScreen">
+    <span>{{ $t('module.information.quiz.participant.waiting') }}...</span>
+    <span
+        id="loading"
+        v-loading="true"
+        element-loading-background="rgba(0, 0, 0, 0)"
+    ></span>
+  </div>
+  <div id="QuizImageBackground">
+    <div id="QuizImageContainer">
+      <img
+          src="../../../../assets/illustrations/Quiz/QuizImage.jpg"
+          alt="quizImage"
+          class="QuizImage"
+      />
+    </div>
+  </div>
+  <ParticipantModuleDefaultContainer :task-id="taskId" :module="moduleName" id="PMDC">
+    <div id="preloader"></div>
     <template #footer>
       <span class="previousNext">
         <el-button
@@ -9,6 +27,7 @@
           :disabled="!hasPreviousQuestion"
           v-if="!moderatedQuestionFlow"
           @click="goToPreviousQuestion"
+          :class="{submitScreenButton: submitScreen}"
         >
           {{ $t('module.information.quiz.participant.previous') }}
         </el-button>
@@ -17,11 +36,19 @@
           class="el-button--submit"
           native-type="submit"
           :disabled="!hasNextQuestion"
-          v-if="!moderatedQuestionFlow"
+          v-if="!moderatedQuestionFlow && hasNextQuestion"
           @click="goToNextQuestion"
         >
           {{ $t('module.information.quiz.participant.next') }}
         </el-button>
+        <el-button
+            type="primary"
+            class="el-button--submit"
+            native-type="submit"
+            :disabled="hasNextQuestion"
+            v-if="!moderatedQuestionFlow && !hasNextQuestion && !submitScreen"
+            @click="goToSubmitScreen"
+        > {{ $t('module.information.quiz.participant.submit') }} </el-button>
       </span>
     </template>
     <PublicBase
@@ -33,6 +60,7 @@
       v-on:changeQuizState="getTask"
       v-on:changePublicQuestion="(id) => (activeQuestionId = id)"
       v-on:changePublicAnswers="(answers) => (publicAnswerList = answers)"
+      v-if="!submitScreen"
     >
       <template #answers>
         <el-space direction="vertical" class="fill">
@@ -68,6 +96,9 @@
         </span>
       </template>
     </PublicBase>
+    <div id="submitScreen" v-if="submitScreen">
+      <span>Thank you for the vote. You can go back and edit your answers if you want to.</span>
+    </div>
   </ParticipantModuleDefaultContainer>
 </template>
 
@@ -116,6 +147,28 @@ export default class Participant extends Vue {
 
   QuestionPhase = QuestionPhase;
 
+  submitScreen = false;
+
+  get loading(): boolean {
+    let element = document.getElementById('loadingScreen');
+
+    if (element != null && !element.classList.contains('zeroOpacity')) {
+
+      var preload = document.getElementById('preloader');
+      preload?.classList.add('PreloadSprites');
+
+      setTimeout(() => preload?.classList.remove('PreloadSprites'), 1000);
+      setTimeout(() => element?.classList.add('zeroOpacity'), 1000);
+      setTimeout(() => element?.classList.add('hidden'), 3000);
+      return true;
+    }
+    return false;
+  }
+
+  mounted(): void {
+    this.loading;
+  }
+
   get isActive(): boolean {
     if (this.task) return timerService.isActive(this.task);
     return false;
@@ -138,7 +191,15 @@ export default class Participant extends Vue {
   }
 
   goToNextQuestion(): void {
+    if (this.submitScreen) {
+      this.submitScreen = false;
+    }
     if (this.hasNextQuestion) this.activeQuestionIndex++;
+  }
+
+  goToSubmitScreen(): void {
+    this.activeQuestionIndex++;
+    this.submitScreen = true;
   }
 
   get hasPreviousQuestion(): boolean {
@@ -146,6 +207,9 @@ export default class Participant extends Vue {
   }
 
   goToPreviousQuestion(): void {
+    if (this.submitScreen) {
+      this.submitScreen = false;
+    }
     if (this.hasPreviousQuestion) this.activeQuestionIndex--;
   }
 
@@ -266,6 +330,10 @@ export default class Participant extends Vue {
   }
 }
 
+.el-footer {
+  height: auto;
+}
+
 .question {
   border: 1px solid var(--color-primary);
   border-radius: var(--border-radius);
@@ -298,4 +366,150 @@ export default class Participant extends Vue {
   padding: 1rem 2rem;
   justify-content: left;
 }
+
+#PMDC {
+  border-radius: 30px 30px 0 0;
+  position: absolute;
+  top: 30%;
+  min-height: 70%;
+
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+
+  z-index: 1;
+}
+
+#PMDC::v-deep .el-steps {
+  margin-bottom: 3%;
+}
+
+div#loadingScreen {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+
+  max-width: 760px;
+
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+
+  background-color: var(--color-darkblue);
+
+  display: flex;
+  justify-items: center;
+  align-items: center;
+  flex-direction: column;
+
+  opacity: 1;
+  z-index: 2;
+}
+
+div#loadingScreen > span {
+  width: 70%;
+  text-align: center;
+  color: white;
+  font-size: var(--font-size-large);
+  position: relative;
+  margin: auto auto 0;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+
+div#loadingScreen > span#loading {
+  margin-top: 50px;
+  margin-bottom: auto;
+}
+
+div#loadingScreen > span#loading::v-deep .path {
+  stroke: white;
+  stroke-width: 4;
+}
+
+.zeroOpacity {
+  opacity: 0 !important;
+  transition: 2s;
+}
+
+.hidden {
+  display: none !important;
+}
+
+#QuizImageBackground {
+  position: absolute;
+
+  max-width: inherit;
+  height: 80%;
+
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+
+  background-image: url('../../../../assets/illustrations/Voting/StarsSpace.png');
+  background-size: contain;
+
+  z-index: 0;
+}
+
+#QuizImageContainer {
+  position: absolute;
+
+  height: 23%;
+
+  top: 3%;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.QuizImage {
+  position: absolute;
+  height: 100%;
+
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+
+  border-radius: 20px;
+  border: 10px solid var(--color-darkblue-light);
+}
+
+.el-space::v-deep .el-button.el-button--primary.el-button--default.link.outline-thick:hover {
+  background-color: var(--color-darkblue);
+  border-color: var(--color-darkblue-light);
+}
+
+.el-space::v-deep .fa-circle-check > path {
+  fill: var(--color-yellow);
+}
+
+.el-space::v-deep .fa-circle > path {
+  fill: var(--color-darkblue-light);
+}
+
+#submitScreen {
+  margin-top: 10%;
+  text-align: center;
+}
+
+.el-button.submitScreenButton {
+  width: 100%;
+  text-align: center;
+  display: flex;
+
+  justify-content: center;
+  justify-items: center;
+  align-items: center;
+  align-content: center;
+}
+
 </style>
