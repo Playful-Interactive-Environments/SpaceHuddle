8e5743398191fb676ab1e1a555601d96365af7b3
Aschauer Andrea <P41499@fhooe.at>
2023-03-06 11:26:54 +0100

    #backend: fix session delete bug

--- api/src/Domain/Session/Service/SessionDeleter.php
+++ api/src/Domain/Session/Service/SessionDeleter.php
@@ -11,4 +11,25 @@ class SessionDeleter
 {
     use ServiceDeleterTrait;
     use SessionServiceTrait;
+
+    /**
+     * Executes the repository instructions assigned to the service.
+     *
+     * @param array $data Input data from the request.
+     *
+     * @return array|object|null Repository answer.
+     */
+    protected function serviceExecution(
+        array $data
+    ): array|object|null {
+        $id = $data["id"];
+        $parentId = $this->repository->deleteById($id, false);// Commit all changes
+
+        $entityName = $this->repository->getEntityName();
+        return (object)[
+            "state" => "Success",
+            "message" => "$entityName was successfully deleted.",
+            "parentId" => $parentId
+        ];
+    }
 }
