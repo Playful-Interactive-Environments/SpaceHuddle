c4765a15447d57cc0c05b627df5b0b2c03e0ae5b
Aschauer Andrea <P41499@fhooe.at>
2023-03-22 09:28:59 +0100

    #backend: fix sessen clone bug

--- api/src/Domain/Session/Repository/SessionRepository.php
+++ api/src/Domain/Session/Repository/SessionRepository.php
@@ -632,6 +632,28 @@ class SessionRepository implements RepositoryInterface
     }*/
 
     /**
+     * Clone entity row.
+     * @param string $oldId Old table primary key
+     * @param string | null $newParentId New parent key value to be inserted
+     * @return string | null The new created entity id
+     */
+    public function clone(string $oldId, ?string $newParentId = null, bool $insertDependencies = true): ?string
+    {
+        $newId = $this->queryFactory->newClone(
+            $this->getEntityName(),
+            ["id" => $oldId],
+            $this->cloneColumns(),
+            null,
+            null
+        );
+
+        if ($insertDependencies && $newId) {
+            $this->cloneDependencies($oldId, $newId);
+        }
+        return $newId;
+    }
+
+    /**
      * Include dependent data.
      * @param string $oldId Old table primary key
      * @param string $newId Old table primary key
