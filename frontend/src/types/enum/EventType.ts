d7ca1f836ada1d1477a95e5dcb55b767a4d760fa
Andrea Aschauer <andrea.aschauer@fh-hagenberg.at>
2023-06-09 16:03:04 +0200

    #frontend: game framework

--- frontend/src/types/enum/EventType.ts
+++ frontend/src/types/enum/EventType.ts
@@ -3,4 +3,5 @@ export enum EventType {
   CHANGE_SETTINGS = 'changeSettings',
   CHANGE_TUTORIAL = 'changeTutorial',
   CHANGE_SIDEBAR_VISIBILITY = 'changeSidebarVisibility',
+  REGISTER_GAME_OBJECT = 'registerGameObject',
 }
