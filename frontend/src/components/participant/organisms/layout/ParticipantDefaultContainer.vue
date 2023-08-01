55d5bafb75059c3460a720ec8151877c401303cc
Paul Huemer <paul.huemer01@gmail.com>
2023-08-01 13:18:20 +0200

    #frontend: minor color palette change

--- frontend/src/components/participant/organisms/layout/ParticipantDefaultContainer.vue
+++ frontend/src/components/participant/organisms/layout/ParticipantDefaultContainer.vue
@@ -67,7 +67,7 @@ export default class ParticipantDefaultContainer extends Vue {
   &__background {
     background: var(--color-dark-contrast);
     background-image: var(--participant-background-image);
-    //background-size: contain;
+    background-size: cover;
     //background-repeat: no-repeat;
     mask-image: radial-gradient(
         circle farthest-corner at 100% 100%,
