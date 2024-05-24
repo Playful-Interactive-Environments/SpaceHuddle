34cb240b085d2afc6cac0fe6203e480cf9ae07d4
Andrea Aschauer <andrea.aschauer@fh-hagenberg.at>
2024-05-24 13:17:45 +0200

    #frontend: quiz - fix bug

--- frontend/src/modules/information/quiz/organisms/QuizResult.vue
+++ frontend/src/modules/information/quiz/organisms/QuizResult.vue
@@ -180,16 +180,11 @@ export default class QuizResult extends Vue {
         ? (maxVote.idea.order as number)
         : this.voteResult.length - 1;
       for (let i = min; i <= max; i++) {
-        const color = (
-          color1.mix(color2, (1 / (max + 1)) * (i + 1), {
-            space: 'lch',
-            outputSpace: 'srgb',
-          }) as any
-        ).coords as number[];
-        let hexColor = '#';
-        color.forEach(
-          (coord) => (hexColor += Math.round(coord * 255).toString(16))
-        );
+        const color = color1.mix(color2, (1 / max) * i, {
+          space: 'lch',
+          outputSpace: 'srgb',
+        }) as any;
+        const hexColor = color.toString({ format: 'hex', collapse: false });
         legend.push({
           color: hexColor,
           name: (i + 1).toString(),
