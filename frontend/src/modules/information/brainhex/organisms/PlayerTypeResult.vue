dc8a8dd29d3b5bf1bd7c0dd64c16c191c177c625
Aschauer Andrea <P41499@fhooe.at>
2024-05-27 14:23:11 +0200

    #frontend: brainhex - statistic

--- frontend/src/modules/information/brainhex/organisms/PlayerTypeResult.vue
+++ frontend/src/modules/information/brainhex/organisms/PlayerTypeResult.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="chartContainer" :style="{ height: `${chartHeight}rem` }">
     <Bar
+      v-if="!showDetails"
       id="chartRef"
       ref="chartRef"
       :data="chartData"
@@ -43,13 +44,72 @@
         },
       }"
     />
+    <Bubble
+      v-else
+      id="chartRef"
+      ref="chartRef"
+      :data="chartData"
+      :options="{
+        scales: {
+          x: {
+            min: -0.5,
+            max: 6.5,
+            title: {
+              display: true,
+              text: $t('module.information.brainhex.participant.secondary'),
+            },
+            ticks: {
+              color: contrastColor,
+              precision: 0,
+              callback: (value, index) => getPlayTypeName(value),
+            },
+          },
+          y: {
+            min: -0.5,
+            max: 6.5,
+            title: {
+              display: true,
+              text: $t('module.information.brainhex.participant.primary'),
+            },
+            ticks: {
+              color: contrastColor,
+              precision: 0,
+              callback: (value, index) => getPlayTypeName(value),
+            },
+          },
+        },
+        plugins: {
+          legend: {
+            display: false,
+          },
+          tooltip: {
+            callbacks: {
+              label: (context) => {
+                context.formattedValue = (
+                  context.raw.r / zoomFactor
+                ).toString();
+                return context.formattedValue;
+              },
+              title: (context) => {
+                if (context[0].raw) {
+                  return `${getPlayTypeName(
+                    context[0].raw.y
+                  )} / ${getPlayTypeName(context[0].raw.x)}`;
+                }
+                return '';
+              },
+            },
+          },
+        },
+      }"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import { Prop, Watch } from 'vue-property-decorator';
-import { Bar } from 'vue-chartjs';
+import { Bar, Bubble } from 'vue-chartjs';
 import { v4 as uuidv4 } from 'uuid';
 import { delay } from '@/utils/wait';
 import * as themeColors from '@/utils/themeColors';
@@ -64,6 +124,7 @@ import { getRandomColorList } from '@/utils/colors';
 @Options({
   components: {
     Bar,
+    Bubble,
   },
 })
 
@@ -75,6 +136,7 @@ export default class PlayerTypeResult extends Vue {
   @Prop({ default: false }) readonly showAll!: boolean;
   @Prop({ default: false }) readonly showDetails!: boolean;
 
+  readonly zoomFactor = 5;
   chartData: any = {
     labels: [],
     datasets: [],
@@ -91,6 +153,16 @@ export default class PlayerTypeResult extends Vue {
     const minHeight = headHeight + itemHeight * 2;
     if (calcHeight < minHeight) return minHeight;
     return calcHeight;
+  }
+
+  getPlayTypeName(index: number): string {
+    if (Number.isInteger(index)) {
+      const playerTypeList = Object.values(PlayerType);
+      return this.$t(
+        `module.information.brainhex.enum.playerType.${playerTypeList[index]}`
+      );
+    }
+    return '';
   }
 
   unmounted(): void {
@@ -145,7 +217,7 @@ export default class PlayerTypeResult extends Vue {
   get resultData(): any {
     const datasets: any[] = [];
     if (this.showDetails) {
-      const randomColors = getRandomColorList(7);
+      /*const randomColors = getRandomColorList(7);
       let index = 0;
       for (const playerType of Object.values(PlayerType)) {
         datasets.push({
@@ -161,7 +233,33 @@ export default class PlayerTypeResult extends Vue {
           color: themeColors.getContrastColor(),
         });
         index++;
+      }*/
+      const playerTypeList = Object.values(PlayerType);
+      const data: { x: number; y: number; r: number }[] = [];
+      for (let i = 0; i < playerTypeList.length; i++) {
+        const playerType1 = playerTypeList[i];
+        for (let j = 0; j < playerTypeList.length; j++) {
+          const playerType2 = playerTypeList[j];
+          const value = this.playerTypeCombinedCount[playerType1][playerType2];
+          if (value) {
+            data.push({
+              x: j,
+              y: i,
+              r: value * this.zoomFactor,
+            });
+          }
+        }
       }
+      datasets.push({
+        data: data,
+        borderRadius: 5,
+        borderColor: themeColors.getYellowColor(),
+        backgroundColor: themeColors.convertToRGBA(
+          themeColors.getYellowColor(),
+          0.5
+        ),
+        color: themeColors.getContrastColor(),
+      });
     } else if (this.showAll) {
       const rateColors: string[] = [];
       const color1 = new Color(themeColors.getGreenColor());
