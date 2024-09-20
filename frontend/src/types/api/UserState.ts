35fa44ed52157e773120455c58c931fb7148e5d9
Andrea Aschauer <andrea.aschauer@fh-hagenberg.at>
2024-09-20 10:53:25 +0200

    #frontend: super admin

--- frontend/src/types/api/UserState.ts
+++ frontend/src/types/api/UserState.ts
@@ -1,6 +1,7 @@
 export interface UserState {
   id: string;
   username: string;
+  creationDate: string;
   confirmed: boolean;
   ownSessions: number;
   sharedSessions: number;
--- frontend/src/views/moderator/Admin/UserList.vue
+++ frontend/src/views/moderator/Admin/UserList.vue
@@ -3,38 +3,85 @@
     :currentRouteTitle="$t('moderator.view.userList.header')"
   >
     <template v-slot:content>
-      <div class="confirm__content full-height-header">
-        <h1>{{ $t('moderator.view.userList.header') }}</h1>
+      <div ref="content" class="confirm__content full-height-header">
+        <div class="level">
+          <div class="level-left">
+            <h1 class="level-item">
+              {{ $t('moderator.view.userList.header') }}
+            </h1>
+          </div>
+          <div class="level-right">
+            <div class="level-item">
+              <font-awesome-icon class="icon" icon="users" />
+              {{ userList.length }}
+            </div>
+            <div class="level-item">
+              <font-awesome-icon
+                class="icon"
+                icon="check"
+                style="color: var(--color-green)"
+              />
+              {{ userList.filter((item) => item.confirmed).length }}
+            </div>
+            <div class="level-item">
+              <font-awesome-icon
+                class="icon"
+                icon="xmark"
+                style="color: var(--color-red)"
+              />
+              {{ userList.filter((item) => !item.confirmed).length }}
+            </div>
+            <div class="level-item">
+              <font-awesome-icon class="icon" icon="file" />
+              {{ userList.reduce((a, b) => a + b.ownSessions, 0) }}
+            </div>
+          </div>
+        </div>
         <el-table
           v-if="userList && userList.length > 0"
           :data="userList"
           style="width: 100%"
-          max-height="250"
+          :max-height="maxHeight - 50"
         >
           <el-table-column
             prop="username"
             :label="$t('moderator.view.userList.username')"
+            sortable
+          />
+          <el-table-column
+            prop="creationDate"
+            :label="$t('moderator.view.userList.creationDate')"
+            sortable
           />
           <el-table-column
             prop="confirmed"
             :label="$t('moderator.view.userList.confirmed')"
+            sortable
           >
             <template #default="scope">
               <font-awesome-icon
                 v-if="scope.row.confirmed"
                 class="icon"
                 icon="check"
+                style="color: var(--color-green)"
               />
-              <font-awesome-icon v-else class="icon" icon="xmark" />
+              <font-awesome-icon
+                v-else
+                class="icon"
+                icon="xmark"
+                style="color: var(--color-red)"
+              />
             </template>
           </el-table-column>
           <el-table-column
             prop="ownSessions"
             :label="$t('moderator.view.userList.ownSessions')"
+            sortable
           />
           <el-table-column
             prop="sharedSessions"
             :label="$t('moderator.view.userList.sharedSessions')"
+            sortable
           />
           <el-table-column width="170">
             <template #default="scope">
@@ -67,10 +114,13 @@ import ModeratorNavigationLayout from '@/components/moderator/organisms/layout/M
 /* eslint-disable @typescript-eslint/no-explicit-any*/
 export default class UserList extends Vue {
   userList: UserState[] = [];
+  maxHeight = 100;
 
   userCash!: cashService.SimplifiedCashEntry<UserState[]>;
   mounted(): void {
     this.userCash = userService.registerGetUserList(this.updateList);
+    const content = this.$refs.content as HTMLElement;
+    this.maxHeight = content.clientHeight;
   }
 
   unmounted(): void {
@@ -94,5 +144,19 @@ export default class UserList extends Vue {
 h1 {
   font-weight: var(--font-weight-semibold);
   font-size: var(--font-size-large);
+}
+
+.icon {
+  color: var(--color-yellow);
+}
+
+.level {
+  padding-bottom: 1rem;
+}
+
+.level-right {
+  .level-item {
+    padding-left: 0.5rem;
+  }
 }
 </style>
